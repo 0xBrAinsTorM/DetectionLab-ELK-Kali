920e622fb7309e7321b4e81bce5aa9a31c8f6121
0xBrAinsTorM <BrAinsTorM@quakenet.biz>
2020-09-01 14:25:52 +0200

    Update bootstrap.sh

--- Vagrant/bootstrap.sh
+++ Vagrant/bootstrap.sh
@@ -78,16 +78,16 @@ fix_eth1_static_ip() {
   # Fix eth1 if the IP isn't set correctly
   ETH1_IP=$(ip -4 addr show eth1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
   if [ "$ETH1_IP" != "192.168.38.105" ]; then
-    #echo "Incorrect IP Address settings detected. Attempting to fix."
-    #ifdown eth1
-    #ip addr flush dev eth1
-    #ifup eth1
+    echo "Incorrect IP Address settings detected. Attempting to fix."
+   # ifdown eth1
+   # ip addr flush dev eth1
+   # ifup eth1
     ETH1_IP=$(ifconfig eth1 | grep 'inet addr' | cut -d ':' -f 2 | cut -d ' ' -f 1)
     if [ "$ETH1_IP" == "192.168.38.105" ]; then
-    #  echo "[$(date +%H:%M:%S)]: The static IP has been fixed and set to 192.168.38.105"
+      echo "[$(date +%H:%M:%S)]: The static IP has been fixed and set to 192.168.38.105"
     else
       echo "[$(date +%H:%M:%S)]: Failed to fix the broken static IP for eth1. Exiting because this will cause problems with other VMs."
-    #  exit 1
+   #   exit 1
     fi
   fi
 
